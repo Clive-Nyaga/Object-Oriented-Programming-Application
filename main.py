17a67612805f1b6cafcbbca332892fb915753c05
Clive Nyaga Kagotho <clive.kagotho@student.moringaschool.com>
2025-08-14 15:57:37 +0300

    Adds, apply_discount(), appends instances to a list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Item:    
+    pay_rate = 0.8 # The pay rate after 20% discount
+    all = []
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         # assert len(name) > 0, f"Name {name} is not greater than zero!"
@@ -10,9 +12,29 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
 item1 = Item("Phone", 100, 5)
 
 item2 = Item("Laptop", 1000, 3)
+
+item3 = Item("Cable", 10, 5)
+
+item4 = Item("Mouse", 50, 5)
+
+item5 = Item("Keyboard", 75, 5)
+
+# for item in Item.all:
+#     print(f"Item name: {item.name}, Item price: {item.price}, Item quantity: {item.quantity}")
+
+print(Item.all)
