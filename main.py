757e5d9ea2c7921e3da35e894c10818b1cf9f552
Clive Nyaga Kagotho <clive.kagotho@student.moringaschool.com>
2025-08-14 15:05:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+class Item:    
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        # assert len(name) > 0, f"Name {name} is not greater than zero!"
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+item1 = Item("Phone", 100, 5)
+
+item2 = Item("Laptop", 1000, 3)
