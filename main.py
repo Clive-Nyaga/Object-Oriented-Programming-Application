2c1e27e6972d0bcd772186a54ee9012f3fcb65fc
Clive Nyaga Kagotho <clive.kagotho@student.moringaschool.com>
2025-08-16 13:25:28 +0300

    Adds Phone class

--- main.py
+++ main.py
@@ -35,10 +35,52 @@ class Item:
                 quantity=int(item.get('quantity')),
             )
 
+    @staticmethod
+    def is_integer(num):
+        if isinstance(num, float):
+            # Check if a float has a zero decimal part
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
 
     def __repr__(self):
-        return f"Item('{self.name}', {self.price}, {self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
 Item.instantiate_from_csv()
 
 print(Item.all)
+
+print(Item.is_integer(5.0))
+print(Item.is_integer(5.5))
+
+class Phone(Item):
+    all = []
+    def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+        # Call to super function to have access to all attributes / methods
+        super().__init__(
+            name, price, quantity
+        )
+
+        # Run validations to the received arguments
+        assert broken_phones >= 0, f"Broken Phones {broken_phones} is not greater than or equal to zero!"
+
+        # Assign to self object
+        self.broken_phones = broken_phones
+
+        # Actions to execute
+        Phone.all.append(self)
+
+        # def __repr__(self):
+            # return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.broken_phones})"
+
+phone1 = Phone("jscPhoneV10", 500, 5, 1)
+# phone1.broken_phones = 1
+print(phone1.calculate_total_price())
+
+phone2 = Phone("jscPhoneV20", 700, 5, 1)
+# phone2.broken_phones = 1
+
+print(Item.all)
+print(Phone.all)
